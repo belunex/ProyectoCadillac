c64ed1ba04084e33390bef4906c85d6d9aa147f7
belunex <rojas.anahi.9190@gmail.com>
2023-09-07 14:51:27 -0400

    Adicion de board a vista de administrador

---
+++ application/views/vistaJefe/board/pie.php
@@ -0,0 +1,54 @@
+<!-- footer content -->
+<footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/Flot/jquery.flot.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="<?php echo base_url();?>/adminlte/vendors/moment/min/moment.min.js"></script>
+    <script src="<?php echo base_url();?>/adminlte/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="<?php echo base_url();?>/adminlte/build/js/custom.min.js"></script>
+	
+  </body>
+</html>
